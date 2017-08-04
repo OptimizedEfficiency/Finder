90b2e3aaa6a07925e15b6d8cfd1e377c0a3f3742
Drew Neely <gpsdrew@gmail.com>
2017-08-04 18:48:08 -0500

    Implemented Login and Registration

--- app/src/main/java/thosekids/com/finder_sohacks4/RegisterActivity.java
+++ app/src/main/java/thosekids/com/finder_sohacks4/RegisterActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,12 +22,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import butterknife.BindView;
 import butterknife.OnClick;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity  {
 
+    @BindView(R.id.name) EditText editTextName;
     @BindView(R.id.email) EditText editTextEmail;
     @BindView(R.id.password) EditText editTextPassword;
-    @BindView(R.id.login) Button login;
-    @BindView(R.id.registerNow) TextView registerNow;
+    @BindView(R.id.register) Button register;
+    @BindView(R.id.loginNow) TextView loginNow;
 
     FirebaseAuth firebaseAuth;
     DatabaseReference databaseReference;
@@ -35,7 +37,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         firebaseAuth = FirebaseAuth.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference();
@@ -44,8 +46,14 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.register)
     public void registerClicked() {
+        String name = editTextName.getText().toString().trim();
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(name)) {
+            Toast.makeText(this, "Please enter name", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         if(TextUtils.isEmpty(email)) {
             Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
@@ -57,10 +65,10 @@ public class LoginActivity extends AppCompatActivity {
             return;
         }
 
-        progressDialog.setMessage("Logging in...");
+        progressDialog.setMessage("Registering User...");
         progressDialog.show();
 
-        firebaseAuth.signInWithEmailAndPassword(email, password)
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -68,11 +76,13 @@ public class LoginActivity extends AppCompatActivity {
                         progressDialog.dismiss();
 
                         if(task.isSuccessful()) {
+                            UserInformation userInformation = new UserInformation(editTextName.getText().toString().trim());
+                            databaseReference.child(FirebaseAuth.getInstance().getCurrentUser().getEmail()).setValue(userInformation);
                             finish();
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         } else {
                             progressDialog.hide();
-                            Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegisterActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
                         }
 
                     }
@@ -81,8 +91,8 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    @OnClick(R.id.registerNow)
+    @OnClick(R.id.loginNow)
     public void loginNowClicked() {
-        startActivity(new Intent(this, RegisterActivity.class));
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
---
+++ app/src/main/java/thosekids/com/finder_sohacks4/UserInformation.java
@@ -0,0 +1,14 @@
+package thosekids.com.finder_sohacks4;
+
+/**
+ * Created by drewneely on 8/4/17.
+ */
+
+public class UserInformation {
+
+    public String name;
+
+    public UserInformation(String name) {
+        this.name = name;
+    }
+}
