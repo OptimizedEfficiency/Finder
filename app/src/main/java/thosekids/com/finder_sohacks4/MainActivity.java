9849db1f1c0c141039e533c626f79272bea3810c
ShreySquared <shreysquared@gmail.com>
2017-08-04 17:25:33 -0500

    MainActivity will link to LoginActivity of not logged in, App Gradle includes Firebase dependencies

--- app/src/main/java/thosekids/com/finder_sohacks4/MainActivity.java
+++ app/src/main/java/thosekids/com/finder_sohacks4/MainActivity.java
@@ -1,16 +1,36 @@
 package thosekids.com.finder_sohacks4;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
+
+    @OnClick(R.id.StartNav)
+    public void toSelectionActivity(){
+        setContentView(R.layout.activity_selection);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser() == null) {
+            startActivity(new Intent(this, LoginActivity.class));
+        }
 
     }
+
 }
