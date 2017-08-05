2e0dd074c7a50f165abb5c5a201d3ad071d14151
Drew Neely <gpsdrew@gmail.com>
2017-08-04 21:11:14 -0500

    Implemented sign out.

--- app/src/main/java/thosekids/com/finder_sohacks4/MainActivity.java
+++ app/src/main/java/thosekids/com/finder_sohacks4/MainActivity.java
@@ -3,16 +3,18 @@ package thosekids.com.finder_sohacks4;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth firebaseAuth;
-
+    private Button signOutBtn;
 
     @OnClick(R.id.StartNav)
     public void toSelectionActivity(){
@@ -26,11 +28,30 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        System.out.println("Running Main Activity");
+
         firebaseAuth = FirebaseAuth.getInstance();
+
+        System.out.println(firebaseAuth.getCurrentUser().getEmail());
+
         if(firebaseAuth.getCurrentUser() == null) {
+            System.out.println("No user logged in. Switching to Login Activity.");
             startActivity(new Intent(this, LoginActivity.class));
         }
 
+        signOutBtn = (Button) findViewById(R.id.signOutBtn);
+
+        signOutBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == signOutBtn) {
+            System.out.println("Logging out");
+            firebaseAuth.signOut();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
     }
 
 }
