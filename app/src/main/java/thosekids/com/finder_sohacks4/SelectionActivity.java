6e965218b4906a1021790d6d9e4991b4c2c380d4
ShreySquared <shreysquared@gmail.com>
2017-08-04 21:41:02 -0500

    Added ListView to Selection Activity

--- app/src/main/java/thosekids/com/finder_sohacks4/SelectionActivity.java
+++ app/src/main/java/thosekids/com/finder_sohacks4/SelectionActivity.java
@@ -3,9 +3,8 @@ package thosekids.com.finder_sohacks4;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class SelectionActivity extends AppCompatActivity {
+public class SelectionActivity extends AppCompatActivity{
 
-    //@BindView(R.id.userList)
 
 
     @Override
